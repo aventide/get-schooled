c5abdf93dc5e6dfff17e2b6c0fa3a1a96fa99c8d
Alex Staples <ajstaples@gmail.com>
2022-02-03 20:46:35 -0500

    score dialog and trying dots for movement

--- src/App.js
+++ src/App.js
@@ -55,22 +55,22 @@ function App() {
   const [bank, setBank] = useState(shuffle(Object.values(tileSet)));
   const [availableBank, setAvailableBank] = useState([]);
   const [legalMoveSpots, setLegalMoveSpots] = useState([]);
+  const [scores, setScores] = useState({
+    color: 0,
+    animal: 0
+  });
 
   useEffect(() => {
     setAvailableBank(take(bank, 6))
-    console.clear();
-    console.log(
-      "animalScore: ",
-      calculateScore(tileSet, board, "animal")
-    );
-    console.log(
-      "colorScore: ",
-      calculateScore(tileSet, board, "color")
-    );
-    if(bank.length === 0){
-      console.log(
-        calculateScore(tileSet, board)
-      );
+    setScores({
+      animal: calculateScore(tileSet, board, "animal"),
+      color: calculateScore(tileSet, board, "color")
+    })
+    if (bank.length === 0) {
+      setScores({
+        animal: calculateScore(tileSet, board, "animal"),
+        color: calculateScore(tileSet, board, "color")
+      })
     }
   }, [bank])
 
@@ -133,23 +133,62 @@ function App() {
 
   return (
     <div className="h-screen bg-indigo-100 w-full flex justify-center items-center flex-col">
-      <div className="w-full md:w-3/5 lg:w-2/5 border-8 border-blue-300 rounded-md bg-blue-300 grid grid-cols-6 grid-rows-6 cursor-pointer gap-1">
+
+      {/* <div className="w-full md:w-3/5 lg:w-2/5 border-8 border-blue-300 rounded-md bg-blue-300">
+      </div> */}
+
+      {/* <div className= "w-1/5 flex justify-center items-center border-2 border-blue-500 rounded-md text-blue-500">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+        </svg>
+        <span className='px-1 text-lg'>{bank.length}</span>
+      </div> */}
+
+      <label for="my-modal-2" class="btn btn-primary modal-button">Reveal Score</label>
+      <input type="checkbox" id="my-modal-2" class="modal-toggle" />
+      <div class="modal">
+        <div class="modal-box">
+          <p>
+            {`Animals Score: ${scores.animal}`}
+          </p>
+          <p>
+            {`Colors Score: ${scores.color}`}
+          </p>
+          <div class="modal-action">
+            <label for="my-modal-2" class="btn">Close</label>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-4 w-full md:w-3/5 lg:w-2/5 border-8 border-blue-300 rounded-md bg-blue-300 grid grid-cols-6 grid-rows-6 cursor-pointer gap-1">
         {
           board.flat().map(boardTile => {
             const isLegalMoveTile = legalMoveSpots.find(spot => spot.x === boardTile.x && spot.y === boardTile.y)
-            return <div className={`group ${isLegalMoveTile ? 'bg-blue-400' : 'bg-blue-200'} hover:bg-red-400 hover:text-white aspect-square`} onClick={() => handlePlaceTile(boardTile.x, boardTile.y)}>{
+            return <div className={`group ${isLegalMoveTile ? 'bg-indigo-100' : 'bg-blue-200'} hover:bg-red-400 hover:text-white aspect-square`} onClick={() => handlePlaceTile(boardTile.x, boardTile.y)}>{
               boardTile.occupyingTile &&
               (
                 <div className={boardTile.occupyingTile !== targetTile ? "" : "opacity-25"}>
                   <Tile animal={tileSet[boardTile.occupyingTile].animal} color={tileSet[boardTile.occupyingTile].color} onSelect={() => handleSelectTile(boardTile.occupyingTile)} />
                 </div>
               )
-            }</div>
+            }
+              {
+                isLegalMoveTile && (
+                  <div className='h-full w-full flex justify-center items-center'>
+                    <svg height="20" width="20" className='fill-blue-300'>
+                      <circle cx="10" cy="10" r="5"/>
+                    </svg>
+                  </div>
+                )
+              }
+            </div>
           })
         }
       </div>
 
-      <div className="mt-12 flex flex-row justify-evenly items-center w-full px-10">
+
+      {/* put a badge on the dock for total left */}
+      <div className="mt-4 flex flex-row justify-evenly items-center w-full">
         {availableBank.length > 0 && <div className="w-full md:w-3/5 lg:w-2/5 border-8 border-blue-300 rounded-md bg-blue-300 grid grid-cols-6 grid-rows-1 cursor-pointer gap-1">
           {
             availableBank.map((abItem) => (
