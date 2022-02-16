2064a421cc604948149c5973f2b84628f46f3373
Alex Staples <ajstaples@gmail.com>
2022-02-16 11:26:53 -0500

    buttons adjustments

--- src/App.js
+++ src/App.js
@@ -135,10 +135,10 @@ function App() {
 
   return (
     <div className="h-screen bg-indigo-100 w-full flex items-center flex-col">
-      <div className={`w-full ${isTableTopMode ? '' : "md:w-3/5 lg:w-2/5"} bg-blue-600 flex justify-evenly text-blue-100 font-sspRegular`}>
+      <div className={`w-full ${isTableTopMode ? '' : "md:w-3/5 lg:w-2/5"} bg-blue-600 flex justify-evenly text-white font-sspRegular`}>
         <div className="flex items-center px-4">
           <button>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white hover:opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           </button>
@@ -214,6 +214,9 @@ function App() {
                 </svg>
                 <p className="font-fancy mx-1">{bank.length}</p>
               </div>
+              <button className="border-2 border-white rounded-md py-1 font-fancy text-white flex-1 mx-12 hover:opacity-75">
+              DONE
+              </button>
               <button>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 hover:opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
