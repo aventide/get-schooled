79ccd870270bcb1c030c39048657eedfd0423785
Alex Staples <ajstaples@gmail.com>
2022-02-21 07:25:16 -0500

    refactor header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { shuffle, take } from 'lodash';
 
+import BoardHeader from './components/BoardHeader';
+
 import { getLegalMoveSpots, calculateScore } from './util';
 
 // https://www.svgrepo.com/
@@ -165,31 +167,8 @@ function App() {
 
   return (
     <div className="h-screen bg-indigo-100 w-full flex items-center flex-col">
-      <div className={`w-full ${isTableTopMode ? '' : "md:w-3/5 lg:w-2/5"} bg-blue-600 flex justify-evenly text-white font-sspRegular`}>
-        <div className="flex items-center px-4">
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white hover:opacity-75" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
-            </svg>
-          </button>
-        </div>
-        <div className='flex flex-1 justify-between px-6 py-2'>
-        <div className={`flex items-center justify-between ${turnFor === "animals" ? 'text-orange-200': ""}`}>
-            <span className='pr-6 text-3xl'>{scores.animal}</span>
-            <div className="flex flex-col items-center">
-              <span className='text-xl'>CPU</span>
-              <span className='text-xs font-fancy'>Animals</span>
-            </div>
-          </div>
-          <div className={`flex items-center justify-between ${turnFor === "colors" ? 'text-orange-200': ""}`}>
-            <div className="flex flex-col items-center">
-              <span className='text-xl'>Alex</span>
-              <span className='text-xs font-fancy'>Colors</span>
-            </div>
-            <span className='pl-6 text-3xl'>{scores.color}</span>
-          </div>
-        </div>
-      </div>
+
+      <BoardHeader isTableTopMode={isTableTopMode} scores={scores} turnFor={turnFor}/>
 
       <div className={`mt-4 w-full ${isTableTopMode ? "md:w-4/5" : "md:w-3/5 lg:w-2/5"} border-8 border-blue-300 rounded-md bg-blue-300 grid grid-cols-6 grid-rows-6 cursor-pointer ${isTableTopMode ? "gap-2" : "gap-1"}`}>
         {
