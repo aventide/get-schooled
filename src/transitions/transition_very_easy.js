6262495708d6aa5328f6e80a7bcdb75ee8bc9525
Alex Staples <ajstaples@gmail.com>
2022-06-09 01:27:03 -0400

    very easy transition concept - needs cleanup

--- src/transitions/transition_very_easy.js
+++ src/transitions/transition_very_easy.js
@@ -1,6 +1,6 @@
 import { sample, take, uniqBy } from "lodash";
 
-import { getMatchGroups } from "../util";
+import { getMatchGroups, getSpacesAroundGroup } from "../util";
 
 function getRandomEmptySpot(boardBuffer) {
   const emptySpots = boardBuffer.filter((item) => !item.occupyingTile);
@@ -13,7 +13,7 @@ function getRandomAvailableBankTile(bank) {
 }
 
 // get all present variants in all matchGroups for given matchType
-function getBoardMatchTypeVariants(matchGroups, matchType) {
+function getMatchVariants(matchGroups, matchType) {
   const allMatchTiles = matchGroups.flat();
   const uniqueMatchTypes = uniqBy(allMatchTiles, matchType).map(
     (mt) => mt[matchType]
@@ -22,12 +22,11 @@ function getBoardMatchTypeVariants(matchGroups, matchType) {
   return uniqueMatchTypes;
 }
 
-function getMatchingBankTilesForBoardVariants(matchGroups, matchType, bank) {
+function getBankTilesByVariants(matchType, matchVariants, bank) {
   const availableBank = take(bank, 6);
-  const matchTypeVariants = getBoardMatchTypeVariants(matchGroups, matchType);
 
   const matchingBankTiles = availableBank.filter((bankTile) =>
-    matchTypeVariants.includes(bankTile[matchType])
+    matchVariants.includes(bankTile[matchType])
   );
 
   return matchingBankTiles;
@@ -38,27 +37,53 @@ export default function transition_very_easy({ board, bank, turnFor }) {
   const boardBuffer = newBoard.flat();
   const matchType = turnFor === "animals" ? "animal" : "color";
   const matchGroups = getMatchGroups(boardBuffer, matchType);
+  const matchVariants = getMatchVariants(matchGroups, matchType);
 
-  // // by default, choose a random bank tile
+  // by default, choose a random bank tile
   let targetBankTile = getRandomAvailableBankTile(bank);
 
-  const matchingBankTilesForVariants = getMatchingBankTilesForBoardVariants(
-    matchGroups,
+  const bankTilesByVariants = getBankTilesByVariants(
     matchType,
+    matchVariants,
     bank
   );
 
-  // this seems to work, but isn't helpful if the groups are totally surrounded
-  // maybe make a variant of this function that only returns the group if at least one space is free
+  // @todo WOW PLEASE CLEAN THIS UP, JEEZ
 
-  // therefore, need a function getFreeTilesAroundGroup();
+  // just fyi, there are redundant values in here
+  const bankMatchedVariants = bankTilesByVariants.map((bt) => bt[matchType]);
 
   // // if this is the first move, just place a random bank tile on a random spot
-  // if (true || !matchingBankTilesForVariants.length) {
-  //   const randomSpot = getRandomEmptySpot(boardBuffer);
-  //   newBoard[randomSpot.y][randomSpot.x].occupyingTile = targetBankTile.id;
-  // } else {
-  // }
+  if (!bankTilesByVariants.length) {
+    const randomSpot = getRandomEmptySpot(boardBuffer);
+    newBoard[randomSpot.y][randomSpot.x].occupyingTile = targetBankTile.id;
+  } else {
+    const bankMatchedGroups = matchGroups.filter(
+      (mg) =>
+        bankMatchedVariants.includes(mg[0][matchType]) &&
+        getSpacesAroundGroup(boardBuffer, mg).length > 0
+    );
+    const largestBMG = bankMatchedGroups.reduce(
+      (bmg, largest) => (bmg.length > largest.length ? bmg : largest),
+      []
+    );
+
+    if (!largestBMG || !largestBMG.length) {
+      // here we would revert to using random placement
+      alert()
+      const randomSpot = getRandomEmptySpot(boardBuffer);
+      newBoard[randomSpot.y][randomSpot.x].occupyingTile = targetBankTile.id;
+    } else {
+      const largestBMGSpaces = getSpacesAroundGroup(boardBuffer, largestBMG);
+      const randomOfSpaces = sample(largestBMGSpaces);
+      const availableBank = take(bank, 6);
+      targetBankTile = availableBank.find(
+        (bankItem) => bankItem[matchType] === largestBMG[0][matchType]
+      );
+      newBoard[randomOfSpaces.y][randomOfSpaces.x].occupyingTile =
+        targetBankTile.id;
+    }
+  }
 
   return {
     newBoard,
