892e97523ed3c8723fb3c5dc5494b2f4d4f9cd50
Alex Staples <ajstaples@gmail.com>
2022-06-30 00:11:26 -0400

    new header design with player name

--- src/components/BoardHeader.js
+++ src/components/BoardHeader.js
@@ -5,61 +5,37 @@ import { useState } from "react";
 export default function BoardHeader({ scores, turnFor, onBack }) {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  function AnimalPlayerInfo() {
+  function ColorPlayerInfo() {
     return (
       <div
-        className={`flex flex-1 px-2  ${
-          turnFor === "animals" ? "text-orange-200" : "white"
+        className={`flex flex-1 items-center px-2  ${
+          turnFor === "colors" ? "text-orange-200" : "white"
         }`}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-full w-8"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          stroke-width="2"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
-          />
-        </svg>
-        <div className="flex flex-col px-2 w-28">
-          <span className="text-xl">{scores.animal}</span>
-          <span className="text-xs text-ellipsis overflow-hidden whitespace-nowrap">
-            Player
+        <span className="text-2xl border-r-2 pr-2">{scores.color}</span>
+        <div className="flex flex-col flex-1 items-start px-2 w-28">
+          <span className="text-base text-ellipsis overflow-hidden whitespace-nowrap">
+            Colors
           </span>
+          <span className="text-2xs leading-3">Player</span>
         </div>
       </div>
     );
   }
 
-  function ColorPlayerInfo() {
+  function AnimalPlayerInfo() {
     return (
       <div
-        className={`flex flex-1 px-2  flex-row-reverse ${
-          turnFor === "colors" ? "text-orange-200" : "white"
+        className={`flex flex-1 items-center px-2 flex-row-reverse ${
+          turnFor === "animals" ? "text-orange-200" : "white"
         }`}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-full w-8"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a2 2 0 012-2h10a2 2 0 012 2v8a2 2 0 01-2 2h-2.22l.123.489.804.804A1 1 0 0113 18H7a1 1 0 01-.707-1.707l.804-.804L7.22 15H5a2 2 0 01-2-2V5zm5.771 7H5V5h10v7H8.771z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <div className="flex flex-col px-2 w-28 text-right">
-          <span className="text-xl">{scores.color}</span>
-          <span className="text-xs text-ellipsis overflow-hidden whitespace-nowrap">
-            Computer
+        <span className="text-2xl border-l-2 pl-2">{scores.animal}</span>
+        <div className="flex flex-col flex-1 items-end px-2 w-28">
+          <span className="text-base text-ellipsis overflow-hidden whitespace-nowrap">
+            Animals
           </span>
+          <span className="text-2xs leading-3">Player</span>
         </div>
       </div>
     );
@@ -89,8 +65,8 @@ export default function BoardHeader({ scores, turnFor, onBack }) {
         </svg>
       </button>
       <div className="flex-1 flex justify-between">
-        <AnimalPlayerInfo />
         <ColorPlayerInfo />
+        <AnimalPlayerInfo />
       </div>
       <StyledModal
         isOpen={isModalOpen}
